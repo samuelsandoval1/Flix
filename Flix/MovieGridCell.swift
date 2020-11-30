5bcedc1c9dde1e9726afe33be7d7575c822ef77b
Samuel Sandoval <sjsandoval1000@gmail.com>
2020-11-29 20:58:20 -0800

    progress on superhero story

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Samuel Sandoval on 11/29/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
